7bbc36d59f167d8df0c73d73867f5b38829d2ab7
Irtaza Fiaz <irtazab135@gmail.com>
2026-01-12 02:33:37 +0500

    Added kmeans clustering

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -2,9 +2,10 @@ from signal import signal
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 from DataTransformation import LowPassFilter, PrincipalComponentAnalysis
 from TemporalAbstraction import NumericalAbstraction
-
+from FrequencyAbstraction import FourierTransformation
 
 # --------------------------------------------------------------
 # Load data
@@ -90,10 +91,10 @@ for col in predictor_columns:
 # Principal component analysis PCA
 # --------------------------------------------------------------
 
-pca_df = df_lowpass.copy()
+df_pca = df_lowpass.copy()
 PCA = PrincipalComponentAnalysis()
 
-pc_values = PCA.determine_pc_explained_variance(pca_df, predictor_columns)
+pc_values = PCA.determine_pc_explained_variance(df_pca, predictor_columns)
 
 plt.figure(figsize=(10, 5))
 plt.bar(range(1, len(pc_values) + 1), pc_values)
@@ -104,21 +105,23 @@ plt.xticks(range(1, len(pc_values) + 1))
 plt.show()
 
 # From the figure we can see that the first 3 components explain most of the variance.
-pca_df = PCA.apply_pca(pca_df, predictor_columns, number_comp=3)
+df_pca = PCA.apply_pca(df_pca, predictor_columns, number_comp=3)
 
-subset = pca_df[pca_df["set"] == 45]
+subset = df_pca[df_pca["set"] == 45]
 
 # --------------------------------------------------------------
 # Sum of squares attributes
 # --------------------------------------------------------------
 
-df_squared = pca_df.copy()
-acc_r = df_squared["acc_x"]**2 + \
-    df_squared["acc_y"]**2 + df_squared["acc_z"]**2
-gyr_r = df_squared["gyr_x"]**2 + \
-    df_squared["gyr_y"]**2 + df_squared["gyr_z"]**2
+df_squared = df_pca.copy()
+acc_r = df_squared["acc_x"] ** 2 + \
+    df_squared["acc_y"] ** 2 + df_squared["acc_z"] ** 2
+gyr_r = df_squared["gyr_x"] ** 2 + \
+    df_squared["gyr_y"] ** 2 + df_squared["gyr_z"] ** 2
 df_squared["acc_r"] = np.sqrt(acc_r)
 df_squared["gyr_r"] = np.sqrt(gyr_r)
+subset = df_squared[df_squared["set"] == 14]
+subset[["acc_r", "gyr_r"]].plot(subplots=True)
 
 # --------------------------------------------------------------
 # Temporal abstraction
@@ -126,28 +129,113 @@ df_squared["gyr_r"] = np.sqrt(gyr_r)
 
 df_temporal = df_squared.copy()
 NumAbs = NumericalAbstraction()
-predictor_columns = predictor_columns + ["acc_r", "gyr_r"]
+predictor_columns = predictor_columns + ["acc_r", "gyr_y"]
 ws = int(1000 / 200)
 for col in predictor_columns:
     df_temporal = NumAbs.abstract_numerical(df_temporal, [col], ws, "mean")
     df_temporal = NumAbs.abstract_numerical(df_temporal, [col], ws, "std")
-df_temporal
+df_temporal_list = []
+for s in df_temporal["set"].unique():
+    subset = df_temporal[df_temporal["set"] == s].copy()
+    for col in predictor_columns:
+        subset = NumAbs.abstract_numerical(subset, [col], ws, "mean")
+        subset = NumAbs.abstract_numerical(subset, [col], ws, "std")
+    df_temporal_list.append(subset)
+df_temporal = pd.concat(df_temporal_list)
+
+
+subset[["acc_y", "acc_y_temp_mean_ws_5",
+        "acc_y_temp_std_ws_5"]].plot(subplots=True)
 
 # --------------------------------------------------------------
 # Frequency features
 # --------------------------------------------------------------
 
+df_freq = df_temporal.copy().reset_index()
+FreqAbs = FourierTransformation()
+fs = int(1000 / 200)
+ws = int(2800 / 200)
+df_freq = FreqAbs.abstract_frequency(df_freq, ["acc_y"], ws, fs)
+# Visualize results
+subset = df_freq[df_freq["set"] == 15]
+subset[["acc_y"]].plot()
+subset[[
+    "acc_y_max_freq",
+    "acc_y_freq_weighted",
+    "acc_y_pse",
+    "acc_y_freq_1.429_Hz_ws_14",
+    "acc_y_freq_2.5_Hz_ws_14",
+]].plot()
+df_freq_list = []
+for s in df_freq["set"].unique():
+    print(f"Applying Fourier transformations to set {s}")
+    subset = df_freq[df_freq["set"] == s].reset_index(drop=True).copy()
+    subset = FreqAbs.abstract_frequency(subset, predictor_columns, ws, fs)
+    df_freq_list.append(subset)
+df_freq = pd.concat(df_freq_list).set_index("epoch (ms)", drop=True)
 
 # --------------------------------------------------------------
 # Dealing with overlapping windows
 # --------------------------------------------------------------
 
+df_freq = df_freq.dropna()
+df_freq = df_freq.iloc[::2]
 
 # --------------------------------------------------------------
 # Clustering
 # --------------------------------------------------------------
 
+df_cluster = df_freq.copy()
+cluster_columns = ["acc_x", "acc_y", "acc_z"]
+k_values = range(2, 10)
+inertias = []
+for k in k_values:
+    subset = df_cluster[cluster_columns]
+    kmeans = KMeans(n_clusters=k, n_init=20, random_state=0)
+    cluster_labels = kmeans.fit_predict(subset)
+
+    inertias.append(kmeans.inertia_)
+plt.figure(figsize=(10, 10))
+plt.plot(k_values, inertias)
+plt.xlabel("k")
+plt.ylabel("Sum of squared distances")
+plt.show()
+
+kmeans = KMeans(n_clusters=5, n_init=20, random_state=0)
+subset = df_cluster[cluster_columns]
+df_cluster["cluster"] = kmeans.fit_predict(subset)
+
+# Plot clusters
+fig = plt.figure(figsize=(15, 15))
+ax = fig.add_subplot(projection="3d")
+
+for c in df_cluster["cluster"].unique():
+    subset = df_cluster[df_cluster["cluster"] == c]
+    ax.scatter(subset["acc_x"], subset["acc_y"], subset["acc_z"], label=c)
+
+ax.set_xlabel("X-axis")
+ax.set_ylabel("Y-axis")
+ax.set_zlabel("Z-axis")
+plt.legend()
+plt.show()
+
+# Plot accelerometer data to compare
+
+fig = plt.figure(figsize=(15, 15))
+ax = fig.add_subplot(projection="3d")
+
+for l in df_cluster["label"].unique():
+    subset = df_cluster[df_cluster["label"] == l]
+    ax.scatter(subset["acc_x"], subset["acc_y"], subset["acc_z"], label=l)
+
+ax.set_xlabel("X-axis")
+ax.set_ylabel("Y-axis")
+ax.set_zlabel("Z-axis")
+plt.legend()
+plt.show()
 
 # --------------------------------------------------------------
 # Export dataset
 # --------------------------------------------------------------
+
+df_cluster.to_pickle("../../data/processed/03_data_feature_engineered.pkl")
